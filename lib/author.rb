e8264ab29f75593da51e2423348582be4529c054
Nicole Lucien <nicolemlucien@gmail.com>
2019-11-09 18:47:18 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,12 +1,12 @@
-class Author 
-  # attr_accessor :name 
+# class Author 
+#   # attr_accessor :name 
   
-  def name=(author_name)
-  @name = author_name   
-  end 
+#   def name=(author_name)
+#   @name = author_name   
+#   end 
   
-  def name 
-  @name 
-  end
+#   def name 
+#   @name 
+#   end
 
-end
+# end
